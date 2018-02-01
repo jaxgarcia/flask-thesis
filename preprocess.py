1b1ffddb505f52081ca4f7585b513a563d47f060
jaxgarcia <jacks.garcia09@gmail.com>
2018-02-01 11:26:54 +0800

    Preprocess completed

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,7 @@
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
 referenceDict = {}
 experimentalDict = {}
@@ -42,8 +44,56 @@ def stopwordsRemoval(exDict):
 
         exDict[sentenceKey] = tempSentence
 
+    specialCharRemoval(exDict)
+
+
+def specialCharRemoval(exDict):
+    for sentenceKey, experimentalSentence in exDict.items():
+        tempSentence = []
+        for word in experimentalSentence:
+            if word.isalpha() or word.isnumeric():
+                tempSentence.append(word)
+
+        exDict[sentenceKey] = tempSentence
+
+    posTagging(exDict)
+
+
+def posTagging(exDict):
+    for sentenceKey, experimentalSentence in exDict.items():
+        tempSentence = nltk.pos_tag(experimentalSentence)
+        exDict[sentenceKey] = tempSentence
+
+    lemmatization(exDict)
+
+
+def lemmatization(exDict):
+    wordLemmatizer = WordNetLemmatizer()
+
+    for sentenceKey, experimentalSentence in exDict.items():
+        tempSentence = []
+        for word in experimentalSentence:
+            pos = ''
+            wordTuple = [wt for wt in word]
+            wtToList = list(wordTuple)
+            if wtToList[1].startswith('N'):
+                pos = 'n'
+            elif wtToList[1].startswith('J'):
+                pos = 'a'
+            elif wtToList[1].startswith('V'):
+                pos = 'v'
+            elif wtToList[1].startswith('R'):
+                pos = 'r'
+            else:
+                pos = 'n'
+            wtToList[0] = wordLemmatizer.lemmatize(wtToList[0], pos)
+            wlToTuple = tuple(wtToList)
+            tempSentence.append(wlToTuple)
+
+        exDict[sentenceKey] = tempSentence
+
     with open('experimentaldictionary.txt', 'w', encoding='utf-8') as text_file:
-        print(experimentalDict, file=text_file)
+        print(exDict, file=text_file)
 
 
 tokenization(experimentalDict)
